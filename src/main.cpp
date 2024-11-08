f0b36e60ef7d1c50dd2e8d2fe17331d7843ee208
Rustam Bek <rustambek.tech@gmail.com>
2024-11-09 00:41:47 +0100

    Algorithm for flocking behaviour implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
-#define FPS 60
-#define NUM_BOIDS   5
-#define TURN_FACTOR 0.5f
+#define FPS 30
+
+#define NUM_BOIDS         50
+#define VISUAL_RANGE      100.0f
+#define VISUAL_RANGE_SQRD      (VISUAL_RANGE * VISUAL_RANGE)
+#define PROTECTED_RANGE   10.0f
+#define PROTECTED_RANGE_SQRD   (PROTECTED_RANGE * PROTECTED_RANGE)
+#define TURN_FACTOR       0.2f
+#define MATCHING_FACTOR   0.05f
+#define AVOID_FACTOR      0.05f
+#define CENTERING_FACTOR  0.0005f
+#define MIN_SPEED 2
+#define MAX_SPEED 3
 
 #define WIN_WIDTH  800
 #define WIN_HEIGHT 600
 
-#define TOP_MARGIN    WIN_HEIGHT/5.0f
+#define TOP_MARGIN    WIN_HEIGHT/16.0f
 #define BOTTOM_MARGIN WIN_HEIGHT - TOP_MARGIN
-#define LEFT_MARGIN   WIN_WIDTH/2.0f
+#define LEFT_MARGIN   WIN_WIDTH/16.0f
 #define RIGHT_MARGIN  WIN_WIDTH - LEFT_MARGIN
 
 using namespace std;
@@ -22,23 +32,30 @@ int main() {
     window.setFramerateLimit(FPS); // call it once, after creating the window
 
     // Create a circle shape with a radius of 10
-    CircleShape circle(10.0f);
-    circle.setFillColor(Color::Blue);
+    CircleShape circle(1.0f);
+    circle.setFillColor(Color::White);
 
-    // Velocity vector (speed and direction of movement)
-    //Vector2f velocity(10.0f, 10.0f); // Move 0.1 pixels per frame in both X and Y directions
-
+    // Vector of boid elements
     vector<CircleShape> boids(NUM_BOIDS, circle);
+    // Velocity vector (speed and direction of movement)
     vector<Vector2f> velocities;
 
 
-    
-
-    for (uint16_t i = 1; i <= NUM_BOIDS; ++i)
+    for (uint16_t i = 0; i <= NUM_BOIDS; ++i)
     {
         boids[i].setPosition(10.0f, i*50.0f);
         velocities.push_back(Vector2f(i*1.0f, i*1.0f));
     }
+
+    float close_dx = 0;
+    float close_dy = 0;
+    float xvel_avg = 0;
+    float yvel_avg = 0;
+    float xpos_avg = 0;
+    float ypos_avg = 0;
+    float neighbor_cnt = 0;
+    float dx = 0;
+    float dy = 0;
 
     // Main loop: run while the window is open
     while (window.isOpen()) {
@@ -49,33 +66,107 @@ int main() {
                 window.close();
         }
 
-        for(uint16_t i = 0; i < NUM_BOIDS; ++i)
+        
+        for (uint16_t current = 0; current < NUM_BOIDS; ++current)
         {
-            boids[i].move(velocities[i]);
+            /* Zero accumulating variables */
+            close_dx = 0;
+            close_dy = 0;
+            xvel_avg = 0;
+            yvel_avg = 0;
+            xpos_avg = 0;
+            ypos_avg = 0;
+            neighbor_cnt = 0;
 
-            // Top margin
-            if (boids[i].getPosition().y <= TOP_MARGIN)
+            /* Flocking Algorithm */
+            for (uint16_t other = current+1; other < NUM_BOIDS; ++other)
             {
-                velocities[i].y += TURN_FACTOR;
+                // get distance between boids
+                dx = boids[current].getPosition().x - boids[other].getPosition().x;
+                dy = boids[current].getPosition().y - boids[other].getPosition().y;
+
+                float distance_sqrd = dx*dx + dy*dy;
+
+                
+                if (distance_sqrd < PROTECTED_RANGE_SQRD)
+                {
+                    close_dx += dx; /* Separation */
+                    close_dy += dy; /* Separation */
+                }
+
+                else if(distance_sqrd < VISUAL_RANGE_SQRD)
+                {
+                    xvel_avg += velocities[other].x; /* Alignment */
+                    yvel_avg += velocities[other].y; /* Alignment */
+
+                    xpos_avg += boids[other].getPosition().x; /* Cohesion */
+                    ypos_avg += boids[other].getPosition().y; /* Cohesion */
+
+                    neighbor_cnt++;
+                }
+
             }
 
-            // Bottom margin
-            if (boids[i].getPosition().y + boids[i].getRadius() * 2 >= BOTTOM_MARGIN)
+            if (0 < neighbor_cnt)
             {
-                velocities[i].y -= TURN_FACTOR;
+                xvel_avg = xvel_avg/neighbor_cnt; /* Alignment */
+                yvel_avg = yvel_avg/neighbor_cnt; /* Alignment */
+
+                xpos_avg = xpos_avg/neighbor_cnt; /* Cohesion */
+                ypos_avg = ypos_avg/neighbor_cnt; /* Cohesion */
+
+                velocities[current].x += (xpos_avg - boids[current].getPosition().x) * CENTERING_FACTOR; /* Cohesion */
+                velocities[current].y += (ypos_avg - boids[current].getPosition().y) * CENTERING_FACTOR; /* Cohesion */
+
+                velocities[current].x += (xvel_avg - velocities[current].x) * MATCHING_FACTOR; /* Alignment */
+                velocities[current].y += (yvel_avg - velocities[current].y) * MATCHING_FACTOR; /* Alignment */
+            }
+            
+
+            velocities[current].y += close_dy * AVOID_FACTOR; /* Separation */
+            velocities[current].y += close_dy * AVOID_FACTOR; /* Separation */
+
+            /* Screen edges */
+            /* Top margin */
+            if (boids[current].getPosition().y <= TOP_MARGIN)
+            {
+                velocities[current].y += TURN_FACTOR;
             }
 
-            // Left margin
-            if (boids[i].getPosition().x <= LEFT_MARGIN)
+            /* Bottom margin */
+            if (boids[current].getPosition().y + boids[current].getRadius() * 2 >= BOTTOM_MARGIN)
             {
-                velocities[i].x += TURN_FACTOR;
+                velocities[current].y -= TURN_FACTOR;
             }
 
-            // Right margin
-            if (boids[i].getPosition().x + boids[i].getRadius() * 2 >= RIGHT_MARGIN)
+            /* Left margin */
+            if (boids[current].getPosition().x <= LEFT_MARGIN)
             {
-                velocities[i].x -= TURN_FACTOR;
+                velocities[current].x += TURN_FACTOR;
             }
+
+            /* Right margin */ 
+            if (boids[current].getPosition().x + boids[current].getRadius() * 2 >= RIGHT_MARGIN)
+            {
+                velocities[current].x -= TURN_FACTOR;
+            }
+
+            // Speed calculation of current boid
+            float speed = sqrtf(velocities[current].x * velocities[current].x + velocities[current].y * velocities[current].y);
+
+            /* Speed Limits */
+            if (MIN_SPEED > speed)
+            {
+                velocities[current].x = (velocities[current].x / speed) * MIN_SPEED;
+                velocities[current].y = (velocities[current].y / speed) * MIN_SPEED;
+            }
+            if (MAX_SPEED < speed)
+            {
+                velocities[current].x = (velocities[current].x / speed) * MAX_SPEED;
+                velocities[current].y = (velocities[current].y / speed) * MAX_SPEED;
+            }
+
+            boids[current].move(velocities[current]);
         }
 
         // Clear the window with a black color
